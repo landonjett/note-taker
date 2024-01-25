d8737424b955cac289e9e89b51f7694c0516a4ee
landonjett <landonjett@gmail.com>
2024-01-25 12:04:48 -0600

    added some beginner code

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log('server running on port ${PORT}');
+});
+
